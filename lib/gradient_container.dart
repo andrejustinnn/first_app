56b0039d791a47315798b63350a01f175ffb9112
Andre Justin <andrejustin77@gmail.com>
2023-06-30 22:02:16 +0700

    add a function to roll dice

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,23 +1,65 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+//const ini sudah di simpan walaupun blm di execute, karena datanya pasti.
+// final ini untuk value yang baru di calculate / ada hasilnya setelah di execute.
+
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  GradientContainer({super.key, required this.colors});
+
+  GradientContainer.purple({super.key})
+      : colors = const [Colors.indigo, Colors.purple];
+
+  final List<Color> colors;
+
+  var activeDiceImage = 'assets/images/dice-6.png';
+
+  void rollDice() {
+    activeDiceImage = 'assets/images/dice-1.png';
+    print('changing image');
+  }
+
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 48, 12, 110),
-            Color.fromARGB(255, 65, 23, 138)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText(),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const StyledText('Roll the dice'),
+            Image.asset(
+              activeDiceImage,
+              width: 200,
+            ),
+            const SizedBox(
+              // semacam br di html
+              height: 20,
+            ),
+            // ElevatedButton(onPressed: () {}, child: const Text('Roll Dice')) // anonimus
+            ElevatedButton(
+                onPressed: rollDice,
+                style: ElevatedButton.styleFrom(
+                    padding:
+                        const EdgeInsets.all(20), // all padding 3 disemua sisi
+                    // padding: const EdgeInsets.only(
+                    //     top: 20), // all padding 3 disemua sisi
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.black12,
+                    textStyle: const TextStyle(fontSize: 28)),
+                child: const Text('Roll Dice'))
+          ],
+        ),
+
+        // child: StyledText('Hello world!'),
       ),
     );
   }
--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  // const StyledText(String text, {super.key}); gabisa langsung akses text di function build krna tidak terhubung
+  const StyledText(this.text, {super.key}); //langsung terhubung
+
+  final String
+      text; // final karena saat menggunakan widget ini hanya akan assign sekali.
   @override
   Widget build(context) {
-    return const Text(
-      'Hello world!',
-      style: TextStyle(
+    return Text(
+      text,
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 28,
       ),
